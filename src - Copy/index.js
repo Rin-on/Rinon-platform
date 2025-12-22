a6918aff086a8de12e9180231f7140c7f92206c6
unknown <oni73537@gmail.com>
2025-12-22 11:51:29 +0100

    Add react-snap for pre-rendering

---
+++ src - Copy/index.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { hydrate, render } from 'react-dom';
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    // If pre-rendered HTML exists, hydrate it (attach event listeners)
+    hydrate(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        rootElement
+    );
+} else {
+    // Otherwise, render normally
+    render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        rootElement
+    );
+}
